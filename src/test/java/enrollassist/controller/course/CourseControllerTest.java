43972f6f03e4df1f18a2bc9d808fe3dcb5921697
Seyed Mohammad Amin Atyabi <seyedamin1990@gmail.com>
2022-12-19 23:39:22 +0330

    Add addNewCourse API test

--- src/test/java/enrollassist/controller/course/CourseControllerTest.java
+++ src/test/java/enrollassist/controller/course/CourseControllerTest.java
@@ -8,6 +8,8 @@ import ir.proprog.enrollassist.domain.GraduateLevel;
 import ir.proprog.enrollassist.domain.course.AddCourseService;
 import ir.proprog.enrollassist.domain.course.Course;
 import ir.proprog.enrollassist.repository.CourseRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -44,24 +47,58 @@ public class CourseControllerTest {
     private AddCourseService addCourseService;
 
     @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
     private ObjectMapper objectMapper;
+
+    @AfterEach
+    public void tearDown() {
+        courseRepository.deleteAll();
+    }
 
     @Test
     public void allTest_emptyList() throws Exception {
         mvc.perform(get("/courses"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("[]")));
+                .andExpect(content().string("[]"));
     }
 
     @Test
     public void allTest_oneItemInList() throws Exception {
+        Locale.setDefault(Locale.ENGLISH);
         Set<Long> emptySet = new HashSet<>();
-        addCourseService.addCourse(new CourseMajorView(new Course("1234567", "a", 3, GraduateLevel.Undergraduate.name()), emptySet, emptySet));
+        Course course = addCourseService.addCourse(new CourseMajorView(new Course("1234567", "a", 3, GraduateLevel.Undergraduate.name()), emptySet, emptySet));
+        String response = String.format("[{\"courseId\":%d,\"courseNumber\":{\"courseNumber\":\"1234567\"},\"graduateLevel\":\"Undergraduate\",\"courseTitle\":\"a\",\"courseCredits\":3,\"prerequisites\":[]}]", course.getId());
         mvc.perform(get("/courses"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string("[{\"courseId\":1,\"courseNumber\":{\"courseNumber\":\"1234567\"},\"graduateLevel\":\"Undergraduate\",\"courseTitle\":\"a\",\"courseCredits\":3,\"prerequisites\":[]}]"));
+                .andExpect(content().string(response));
+    }
+
+    @Test
+    public void addNewCourseTest_noError() throws Exception {
+        Set<Long> emptySet = new HashSet<>();
+        CourseMajorView courseMajorView = new CourseMajorView(new Course("1234567", "a", 3, GraduateLevel.Undergraduate.name()), emptySet, emptySet);
+        mvc.perform(post("/courses")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(courseMajorView)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"courseNumber\":{\"courseNumber\":\"1234567\"},\"graduateLevel\":\"Undergraduate\",\"courseTitle\":\"a\",\"courseCredits\":3,\"prerequisites\":[]}")));
+    }
+
+    @Test
+    public void addNewCourseTest_badRequest() throws Exception {
+        Set<Long> emptySet = new HashSet<>();
+        addCourseService.addCourse(new CourseMajorView(new Course("1234567", "a", 3, GraduateLevel.Undergraduate.name()), emptySet, emptySet));
+        CourseMajorView courseMajorView = new CourseMajorView(new Course("1234567", "a", 3, GraduateLevel.Undergraduate.name()), emptySet, emptySet);
+        mvc.perform(post("/courses")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(courseMajorView)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
 }
